691a1f547be7a2e26555ecfb63289dd96b9305ad
amauryxd <cdmx2472@amerike.edu.mx>
2024-11-15 22:44:56 -0600

    :gift:

--- Assets/Scripts/IA/GuardianCorrrrruptoIA.cs
+++ Assets/Scripts/IA/GuardianCorrrrruptoIA.cs
@@ -1,7 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
-using Unity.VisualScripting.FullSerializer;
 using UnityEngine;
 
 public class GuardianCorrrrruptoIA : MonoBehaviour
